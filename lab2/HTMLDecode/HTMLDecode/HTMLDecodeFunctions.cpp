20a73955431aeb2b8c88223c4489ff66dd43dcbd
Carapacik <carapaciK@gmail.com>
2020-06-13 20:28:14 +0300

    перенёс костанты из .cpp в .h

--- lab2/HTMLDecode/HTMLDecode/HTMLDecodeFunctions.cpp
+++ lab2/HTMLDecode/HTMLDecode/HTMLDecodeFunctions.cpp
@@ -1,20 +1,5 @@
 #include "HTMLDecodeFunctions.h"
 
-struct Entities 
-{
-    std::string character;
-    std::string entity;
-};
-
-const std::vector<Entities> HTML_ENTITIES = 
-{ 
-    {"&", "&amp;"},
-    {"'", "&apos;"},
-    {">", "&gt;"},
-    {"<", "&lt;"},
-    {"\"", "&quot;"}
-};
-
 std::string ReplaceItemsInString(std::string& html, const std::string& entity, const std::string& character)
 {
     std::string result = "";
